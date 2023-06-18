a9bb23bfc5ac9919585fd71ea7e5aae7d33107d2
catwithtudou <949812478@qq.com>
2023-06-18 18:00:43 +0800

    minigrep:update:update the 12.04

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,7 +25,49 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    //    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+//error[E0106]: missing lifetime specifier
+// --> src/lib.rs:5:51
+//  |
+//5 | pub fn search(query: &str, contents: &str) -> Vec<&str> {
+//    |                                                   ^ expected lifetime
+//parameter
+//|
+//= help: this function's return type contains a borrowed value, but the
+//  signature does not say whether it is borrowed from `query` or `contents`
